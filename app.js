8dd476790e5a9e6a16fa3098f4bc2c82896782ec
huangshuai <786262416@qq.com>
2020-12-12 21:10:12 +0800

    1

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const util = require('./utils/util')
 const api = require('./api')
 const loading = require('./utils/loading')
 const config = require('./utils/config')
+const u8ad = require('./utils/u8ad_sdk.min.js');
 
 App({
     onLaunch: function () {
